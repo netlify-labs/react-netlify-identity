6c4d7229a1672557a12147adc8dc59f027e544ac
sw-yx <swyx@dontemail.me>
2019-05-29 23:58:15 -0400

    fix exampple

---
+++ example-cra/src/useLoading.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function useLoading() {
+  const [isLoading, setState] = React.useState(false);
+  const mount = React.useRef(false);
+  React.useEffect(() => {
+    mount.current = true;
+    return () => void (mount.current = false);
+  }, []);
+  function load<A>(aPromise: Promise<A>) {
+    setState(true);
+    return aPromise.finally(() => mount.current && setState(false));
+  }
+  return [isLoading, load] as [
+    boolean,
+    <A>(aPromise: Promise<A>) => Promise<A>
+  ];
+}
